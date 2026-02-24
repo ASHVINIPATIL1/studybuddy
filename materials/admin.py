17351601c90cead5ab8a4e880a8599d23677d793
ASHVINIPATIL1 <adpatil.mdp@gmail.com>
2026-02-24 13:56:25 +0530

    Registered the models in admin.py, minor changes in landing.html

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import StudyMaterial, ChatMessage
 
-# Register your models here.
+admin.site.register(StudyMaterial)
+admin.site.register(ChatMessage)
